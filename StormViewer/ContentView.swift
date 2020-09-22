b68be040f70d9ad12fca162bd8516ffdba631ab4
Michael Klein <michaelklein@Michaels-MacBook-Pro.local>
2020-09-22 16:26:10 -0400

    initial commit of files

---
+++ StormViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StormViewer
+//
+//  Created by Michael Klein on 9/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
